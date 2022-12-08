fc4361a47d1fc8bb42827223f6541e8a22471b13
DelaraGalehdari <73263822+DelaraGalehdari@users.noreply.github.com>
2022-12-08 22:45:28 +0000

    creating homepage

--- website/src/components/Home/home.js
+++ website/src/components/Home/home.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const home = () => {
-  return <div>home</div>;
+  const [usersinfo, setUsersInfo] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("http://localhost:8080/users");
+      setUsersInfo(res.data.items);
+      console.log(usersinfo);
+    };
+    fetchData();
+  }, [usersinfo]);
+
+  return usersinfo.map((user) => (
+    <div key={user.id}>
+      <div>
+        {user.firstName} {user.lastName}
+      </div>
+      <div>{user.birthDate}</div>
+      <div>{user.gender}</div>
+      <div>{user.created}</div>
+    </div>
+  ));
 };
 
 export default home;
