aa60bc1b6f0954fe474d3d4383122f8ac277ba9f
DelaraGalehdari <73263822+DelaraGalehdari@users.noreply.github.com>
2022-12-08 23:34:58 +0000

    adding pagination

--- website/src/components/Home/home.js
+++ website/src/components/Home/home.js
@@ -1,28 +1,44 @@
 import React, { useState, useEffect } from "react";
+import PaginateUsers from "../PaginateUsers/paginateUsers";
 import axios from "axios";
 
 const home = () => {
   const [usersinfo, setUsersInfo] = useState([]);
+  const [startOffset, setStartOffset] = useState(0);
+  const [endOffset, setEndOffset] = useState(6);
+  const [showUsers, setShowUsers] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get("http://localhost:8080/users");
       setUsersInfo(res.data.items);
-      console.log(usersinfo);
+      setShowUsers(usersinfo.slice(startOffset, endOffset));
+      //   console.log(usersinfo);
     };
     fetchData();
   }, [usersinfo]);
 
-  return usersinfo.map((user) => (
-    <div key={user.id}>
-      <div>
-        {user.firstName} {user.lastName}
-      </div>
-      <div>{user.birthDate}</div>
-      <div>{user.gender}</div>
-      <div>{user.created}</div>
+  //➡️define start and end number of stories for paginations
+  const handlePage = (minIndex, maxIndex) => {
+    setStartOffset(minIndex);
+    setEndOffset(maxIndex);
+  };
+
+  return (
+    <div>
+      <PaginateUsers usersinfo={usersinfo} handlePage={handlePage} />
+      {showUsers.map((user) => (
+        <div key={user.id}>
+          <div>
+            {user.firstName} {user.lastName}
+          </div>
+          <div>{user.birthDate}</div>
+          <div>{user.gender}</div>
+          <div>{user.created}</div>
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
 
 export default home;
---
+++ website/src/components/PaginateUsers/paginateUsers.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+
+const pagination = ({ usersinfo, handlePage }) => {
+  const [usersCount, setUsersCount] = useState(0);
+  const [userOffset, setUserOffset] = useState(0);
+  const usersPerPage = 6;
+
+  useEffect(() => {
+    // Fetch items based of number of page
+    const endOffset = userOffset + usersPerPage;
+    handlePage(userOffset, endOffset);
+    setUsersCount(Math.ceil(usersinfo.length / usersPerPage));
+  }, [userOffset, usersPerPage, usersinfo]);
+
+  const handlePageClick = (event) => {
+    let currentPage = event.selected + 1;
+    const newOffset = (currentPage * usersPerPage) % usersinfo.length;
+    setUserOffset(newOffset);
+  };
+  return (
+    <div>
+      <ReactPaginate
+        nextLabel=">>"
+        previousLabel="<<"
+        breakLabel="..."
+        pageCount={usersCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        renderOnZeroPageCount={null}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
+    </div>
+  );
+};
+
+export default pagination;
